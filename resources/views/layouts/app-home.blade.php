3fa1b30084cead0d9e6efb806aa58bdec4734503
Gerard Nweje <nwejegerard@gmail.com>
2025-05-28 03:41:58 +0000

    removed bootsrap and Updated <head> section to link custom CSS file:
    <link rel="stylesheet" href="{{ asset('assets/main/css/custom.css') }}">

--- resources/views/layouts/app-home.blade.php
+++ resources/views/layouts/app-home.blade.php
@@ -14,7 +14,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/assets/images/icon/yencor-favicon2.ico') }}">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/main/css/custom.css') }}">
+
 
     <x-css-links />
 </head>
